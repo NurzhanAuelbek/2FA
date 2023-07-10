45a35641b2ee520703a9a973224bdb9d4162f3f3
NurzhanAuelbek <82052572+NurzhanAuelbek@users.noreply.github.com>
2023-07-10 23:38:16 +0600

    Добавьте файлы проекта.

---
+++ 2FA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace _2FA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountSid = "Key";
+            var authToken = "Key";
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                from: new Twilio.Types.PhoneNumber("+13254201107"),
+                to: new Twilio.Types.PhoneNumber("+77007001455")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
